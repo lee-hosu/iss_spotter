8ca0efb402426a03b74e568b1aa3dd7cbd7ed4b5
Hosu <sogeumhosu@gmail.com>
2023-07-18 15:03:53 -0700

    Assignment: ISS Spotter II

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -8,3 +8,11 @@ fetchMyIP((error, ip) => {
 
   console.log("It worked! Returned IP:", ip);
 });
+
+// fetchCoordsByIP("70.69.23.104", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It wokred! Returned coordinates:", coordinates);
+// });
